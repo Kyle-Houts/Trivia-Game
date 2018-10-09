ab44e9bc2f39ffe5670d36360a1ff50dd93df3ba
Kyle Houts <kyle.elizabeth.houts@gmail.com>
2018-10-09 08:30:57 -0500

    first commit

---
+++ Trivia-Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia-Game
+//
+//  Created by Kyle Houts on 10/9/18.
+//  Copyright © 2018 Kyle Houts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
